922750023b01dd803e050f3c8965d3fdc249abde
bob <bob@>
2019-04-04 09:31:16 +0300

    Updates

---
+++ precache-manifest.097169c54378447e0c357c959af50504.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5405be38627d87d73313",
+    "url": "/near-app/static/js/runtime~main.03cdebc0.js"
+  },
+  {
+    "revision": "63e4497c8833b1666ca6",
+    "url": "/near-app/static/js/main.dbe58790.chunk.js"
+  },
+  {
+    "revision": "f9e5c1696a9f62862e38",
+    "url": "/near-app/static/js/2.f79b07b6.chunk.js"
+  },
+  {
+    "revision": "63e4497c8833b1666ca6",
+    "url": "/near-app/static/css/main.36980d4f.chunk.css"
+  },
+  {
+    "revision": "f9e5c1696a9f62862e38",
+    "url": "/near-app/static/css/2.6e7b17e7.chunk.css"
+  },
+  {
+    "revision": "fec3aeeda4d98de5239aba8306ad5646",
+    "url": "/near-app/index.html"
+  }
+];
